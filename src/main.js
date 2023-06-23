6afda843eb9ae4d4ec76fdaf497a3be9a8bdb846
sekar N <sekarnagarajan1234@gmail.com>
2023-06-23 22:45:36 +0530

    js

--- src/main.js
+++ src/main.js
@@ -31,6 +31,8 @@ let shopItemData = [
   },
 ];
 
+let basket = [];
+
 let generateShop = () => {
   return (shop.innerHTML = shopItemData
     .map((e) => {
@@ -45,9 +47,9 @@ let generateShop = () => {
       <div class="price-quantity">
         <h2>${price}</h2>
         <div class="button">
-          <i class="bi bi-dash-lg"></i>
-          <div class="quantity">0</div>
-          <i class="bi bi-plus-lg"></i>
+          <i onclick = "decrement(${id})" class="bi bi-dash-lg"></i>
+          <div id=${id} class="quantity">0</div>
+          <i onclick = "increment(${id})" class="bi bi-plus-lg"></i>
         </div>
       </div>
     </div>
@@ -57,3 +59,31 @@ let generateShop = () => {
 };
 
 generateShop();
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+
+  console.log(basket);
+};
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+
+  console.log(basket);
+};
+let update = () => {};
