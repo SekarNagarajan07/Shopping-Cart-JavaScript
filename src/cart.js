b19e6ad4f8af647cbed36d3803af573d037dea96
sekar N <sekarnagarajan1234@gmail.com>
2023-06-24 15:52:07 +0530

    js

--- src/cart.js
+++ src/cart.js
@@ -1,3 +1,6 @@
+let label = document.getElementById("label");
+let shoppingCart = document.getElementById("shopping-cart");
+
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let calculation = () => {
@@ -7,3 +10,44 @@ let calculation = () => {
 };
 
 calculation();
+
+let generateCartItems = () => {
+  if (basket.length !== 0) {
+    return (shoppingCart.innerHTML = basket
+      .map((x) => {
+        let { id, item } = x;
+        let search = shopItemData.find((y) => y.id === id) || [];
+
+        return `
+        <div class="cart-item">
+        <img width="100" src=${search.img} alt="" />
+        <div class="details">
+          <div class="title-price-x">
+              <h4 class="title-price">
+                <p>${search.name}</p>
+                <p class="cart-item-price">$ ${search.price}</p>
+              </h4>
+              <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
+          </div>
+          <div class="buttons">
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">${item}</div>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+          </div>
+          <h3>$ ${item * search.price}</h3>
+        </div>
+      </div>
+      `;
+      })
+      .join(""));
+  } else {
+    shoppingCart.innerHTML = ``;
+    label.innerHTML = `
+    <h2>Cart is Empty</h2>
+    <a href = "index.html">
+    <button class = "HomeBtn">Back To Home</button>
+    </a>`;
+  }
+};
+
+generateCartItems();
