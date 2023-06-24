b191b9e0964726fd51908dfd6e577320a3a379cf
sekar N <sekarnagarajan1234@gmail.com>
2023-06-24 15:06:40 +0530

    js

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemData = [
+  {
+    id: "liegjljwehg",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit architecto, culpa illum autem iste nihil",
+    img: "images/image1.png",
+  },
+  {
+    id: "lkvnlJ",
+    name: "Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit architecto, culpa illum autem iste nihil",
+    img: "images/image2.png",
+  },
+  {
+    id: "KLFNLKvnj",
+    name: "white T-Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit architecto, culpa illum autem iste nihil",
+    img: "images/image3.png",
+  },
+  {
+    id: "djnawjghj",
+    name: "Blue T-Shirt",
+    price: 34,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit architecto, culpa illum autem iste nihil",
+    img: "images/image4.png",
+  },
+];
--- src/main.js
+++ src/main.js
@@ -1,36 +1,5 @@
 let shop = document.getElementById("shop");
 
-let shopItemData = [
-  {
-    id: "liegjljwehg",
-    name: "Casual Shirt",
-    price: 45,
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit architecto, culpa illum autem iste nihil",
-    img: "images/image1.png",
-  },
-  {
-    id: "lkvnlJ",
-    name: "Shirt",
-    price: 25,
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit architecto, culpa illum autem iste nihil",
-    img: "images/image2.png",
-  },
-  {
-    id: "KLFNLKvnj",
-    name: "white T-Shirt",
-    price: 25,
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit architecto, culpa illum autem iste nihil",
-    img: "images/image3.png",
-  },
-  {
-    id: "djnawjghj",
-    name: "Blue T-Shirt",
-    price: 34,
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit architecto, culpa illum autem iste nihil",
-    img: "images/image4.png",
-  },
-];
-
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let generateShop = () => {
