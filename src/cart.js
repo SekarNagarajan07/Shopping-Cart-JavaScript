59196c0d312338e16cfb58136e825fdcc189a5ba
sekar N <sekarnagarajan1234@gmail.com>
2023-06-24 16:06:10 +0530

    js

--- src/cart.js
+++ src/cart.js
@@ -51,3 +51,40 @@ let generateCartItems = () => {
 };
 
 generateCartItems();
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  generateCartItems();
+
+  // console.log(basket);
+  update(selectedItem.id);
+
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+
+  update(selectedItem.id);
+
+  basket = basket.filter((x) => x.item !== 0);
+  generateCartItems();
+  // console.log(basket);
+
+  localStorage.setItem("data", JSON.stringify(basket));
+};
